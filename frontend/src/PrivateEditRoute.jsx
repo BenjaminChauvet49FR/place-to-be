756fc071832938de02385179a4f2cc69a5ea3b78
Benjamin <benjchauvet@gmail.com>
2026-04-08 08:54:45 +0200

    [FRONT][FIXUP] Ne pas toujours rediriger vers la page 'doNotEdit'

--- frontend/src/PrivateEditRoute.jsx
+++ frontend/src/PrivateEditRoute.jsx
@@ -1,14 +1,16 @@
 import { Navigate, Outlet } from "react-router-dom";
 
 import { useAuth } from "./context/AuthContext.jsx";
-import { paths } from "./index.js";
+import NoEditLevel from "./pages/NoEditLevel/index.jsx";
 
 export default function PrivateEditRoute() {
   const { user, loading } = useAuth();
 
   if (loading) return <div>Chargement...</div>;
 
-  if (!user) return <Navigate to={paths.noEditLevel()} />;
+  // if (!user) return <Navigate to={paths.noEditLevel()} />; Good, but it always redirected towards the page "doNotEditLevel"... a bit weird. We can do better, by not changing the address of the page we are on.
+
+  if (!user) return <NoEditLevel />;
 
   return <Outlet />;
 }
