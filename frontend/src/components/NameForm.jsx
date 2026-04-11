b56fde15921b20936d1757db9df929e2daa6ba9b
Benjamin <benjchauvet@gmail.com>
2026-04-11 10:58:16 +0200

    [FRONT][FIXUP] Name form : distinction messages en cas d'erreur + correction déconnexion
    -la deconnexion n'avait pas lieu, c'est corrigé
    -pour la distinction des messages, un test sur le message est
    directement fait :
    c'est forcé par le cas de non-liaison au backend

--- frontend/src/components/NameForm.jsx
+++ frontend/src/components/NameForm.jsx
@@ -22,26 +22,25 @@ function NameForm() {
     if (result.success) {
       // Mettre à jour le contexte global Auth
       login({ username: name }, result.data.access);
-    } else {
+    } else if (
+      result.error.includes(
+        "No active account found with the given credentials",
+      ) // NOTE : pourquoi directement un include message d'erreur ? Eh bien parce que quand on tente de se connecter (connect) on s'arrête avec une exception si le serveur est inactif, mais on continue avec un statut (401, etc...) si le serveur est actif ; je souhaite uniformiser les traitements, donc juste renvoyer l'erreur
+    ) {
       // Afficher le message d'erreur
       setError("Nom ou mot de passe incorrect !");
+    } else if (result.error.includes("Failed to fetch")) {
+      setError("Connexion au serveur non assurée !");
     }
   };
 
   const handleSubmitDisconnect = async (event) => {
     event.preventDefault();
     setError(""); // reset erreur
-    const result = await connect(name, password);
-
-    if (result.success) {
-      // Mettre à jour le contexte global Auth
-      logout();
-
-      // Rediriger vers page d'accueil
-      navigate(paths.home());
-    } else {
-      setError(error);
-    }
+    // Mettre à jour le contexte global Auth
+    logout();
+    // Rediriger vers page d'accueil
+    navigate(paths.home());
   };
 
   return (
