424b26f2a27371fa0427e8bea57bcc22c8f7d13c
Benjamin <benjchauvet@gmail.com>
2026-04-14 16:13:51 +0200

    [FRONT] Mode déplacement des niveaux en cours d'edition (sans impact cote back)

--- frontend/src/pages/EditorMenu/index.jsx
+++ frontend/src/pages/EditorMenu/index.jsx
@@ -5,6 +5,52 @@ import * as saveLoad from "../../logic/saveLoad";
 import { paths } from "../../index";
 import { API_URL } from "../../utils/api.jsx";
 
+import { DndContext, closestCenter } from "@dnd-kit/core";
+
+import {
+  SortableContext,
+  useSortable,
+  verticalListSortingStrategy,
+  arrayMove,
+} from "@dnd-kit/sortable";
+
+import { CSS } from "@dnd-kit/utilities";
+
+// Un élément draggable
+function SortableItem({ id, value }) {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({ id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+    cursor: "grab",
+    backgroundColor: isDragging ? "#ffe082" : "",
+    opacity: isDragging ? 0.9 : 1,
+  };
+
+  return (
+    <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+      {value}
+    </div>
+  );
+}
+
+function findByLevelIndex(pList, pId) {
+  for (var i = 0; i < pList.length; i++) {
+    if (pId === pList[i].id) {
+      return i;
+    }
+  }
+  return -1;
+}
+
 export default function LevelListForEditor() {
   const [loading, setLoading] = useState(false);
   const [levelListJSON, setLevelListJSON] = useState([]);
@@ -46,28 +92,94 @@ export default function LevelListForEditor() {
     }
   }
 
+  // ----------------------------------------------------->
+  // Partie drag & drop
+  const [dragMode, setDragMode] = useState(false);
+  const [formerLevelsList, setFormerLevelsList] = useState(false);
+
+  // Quand on lâche l'élément
+  function handleDragEnd(event) {
+    const { active, over } = event;
+
+    if (!over || active.id === over.id) return;
+
+    const oldIndex = findByLevelIndex(levelListJSON, active.id);
+    const newIndex = findByLevelIndex(levelListJSON, over.id);
+
+    setLevelListJSON(arrayMove(levelListJSON, oldIndex, newIndex));
+  }
+
+  // Bouton commencer, terminer, annuler
+  function startReorder() {
+    setDragMode(true);
+    setFormerLevelsList(levelListJSON.map((level) => level)); // TODO Je sais pas si une telle copie est utile !
+  }
+
+  function finishReorder() {
+    console.log("Nouvel ordre :", levelListJSON);
+
+    // POST vers Django !
+    saveLoad.reorderLevels(levelListJSON.map((level) => level.id));
+    setDragMode(false);
+  }
+
+  function cancelReorder() {
+    setLevelListJSON(formerLevelsList.map((level) => level));
+    setDragMode(false);
+  }
+
+  // ----------------------------------------------------->
+
   return (
-    <div className="level_menu">
+    <div>
       {loading ? (
         <div>Chargement en cours</div>
-      ) : (
-        levelListJSON.map((level, key) => (
-          <div key={`O${key}`}>
-            <Link to={paths.editLevel(level.id)}>
-              {level.name === "" ? "(anonyme)" : level.name}
-            </Link>
-            <span
-              className="delete"
-              onClick={() => deleteLevel(level.id, setLevelListJSON)}
+      ) : dragMode ? (
+        <>
+          <DndContext
+            collisionDetection={closestCenter}
+            onDragEnd={handleDragEnd}
+          >
+            <SortableContext
+              items={levelListJSON}
+              strategy={verticalListSortingStrategy}
             >
-              [Effacer]
-            </span>
-          </div>
-        ))
+              <div className="level_list">
+                {levelListJSON.map((item) => (
+                  <SortableItem key={item.id} id={item.id} value={item.name} />
+                ))}
+              </div>
+            </SortableContext>
+          </DndContext>
+
+          <button onClick={finishReorder}>Terminer</button>
+          <button onClick={cancelReorder}>Annuler</button>
+        </>
+      ) : (
+        <div className="level_list">
+          {levelListJSON.map((level, key) => (
+            <div key={`O${key}`}>
+              <Link to={paths.editLevel(level.id)}>
+                {level.name === "" ? "(anonyme)" : level.name}
+              </Link>
+              <span
+                className="delete"
+                onClick={() => deleteLevel(level.id, setLevelListJSON)}
+              >
+                [Effacer]
+              </span>{" "}
+            </div>
+          ))}
+        </div>
       )}
-      <div className="level_new">
-        <Link to={paths.editNewLevel()}>(Nouveau)</Link>
-      </div>
+      {dragMode ? null : (
+        <>
+          <div className="level_new">
+            <Link to={paths.editNewLevel()}>(Nouveau)</Link>
+          </div>
+          <button onClick={() => startReorder()}>Déplacer niveaux</button>{" "}
+        </>
+      )}
     </div>
   );
 }
