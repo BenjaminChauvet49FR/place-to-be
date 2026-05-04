50668433ba761eee55e862c7f3152ef959621a59
Benjamin <benjchauvet@gmail.com>
2026-05-04 11:43:12 +0200

    [FRONT] Affichage côté front des niveaux terminés dans la quête principale

--- frontend/src/pages/MainQuestMenu/index.jsx
+++ frontend/src/pages/MainQuestMenu/index.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import "../../styles/style.css";
 import { paths } from "../../utils/paths.jsx";
 import { getMainQuestLevels } from "../../utils/api.jsx";
+import validation1 from "../../images/validation_1.png";
 
 export default function Page() {
   const [loading, setLoading] = useState(false);
@@ -26,18 +27,33 @@ export default function Page() {
   }, []);
 
   return (
-    <div className="level_list">
+    <div className="level_list_mq">
       <div>--- Quête principale ---</div>
 
       {loading ? (
         <div>Chargement en cours</div>
       ) : (
         levelListJSON.map((level, key) => (
-          <div key={`O${key}`} role="listitem">
+          <div
+            key={`O${key}`}
+            role="listitem"
+            className={level.completionStatus ? "completed" : "uncompleted"}
+          >
             <span>{level.position} - </span>
             <Link to={paths.playLevelQuest(level.position)}>
               {level.name === "" ? "(anonyme)" : level.name}
             </Link>
+            {level.completionStatus && (
+              <img
+                src={validation1}
+                alt="partiel"
+                style={{
+                  width: "16px",
+                  height: "16px",
+                  verticalAlign: "middle",
+                }}
+              ></img>
+            )}
           </div>
         ))
       )}
