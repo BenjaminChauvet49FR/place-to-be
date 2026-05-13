49252d976731ea11d20bbcd10d19aa9940d5e236
Benjamin <benjchauvet@gmail.com>
2026-05-13 12:17:32 +0200

    [FRONT] Modification de la page MyAdmin pour accueillir le nom des auteurs (+ classement) + le bouton d'ordre

--- frontend/src/pages/AdminOnly/index.jsx
+++ frontend/src/pages/AdminOnly/index.jsx
@@ -5,12 +5,10 @@ export default function Page() {
   const [loading, setLoading] = useState(); // Note : j'avais voulu tout faire sans useState... ça marche pas ;)
   const [dataLevels, setDataLevels] = useState([]);
 
-  // let dataLevels; (Mauvaise idée !)
-
+  // Note rappel : dans le useEffect, obligation de passer par useState et, si appel API, par une fonction en await
   useEffect(() => {
     async function init() {
       setLoading(true);
-      //dataLevels = await loadAllLevels();
       setDataLevels(await loadAllLevels());
       setLoading(false);
     }
@@ -19,19 +17,22 @@ export default function Page() {
 
   return (
     <div>
-      {" "}
       {loading ? (
         <div> Chargement en cours</div>
       ) : (
-        <div>
-          {dataLevels.map((lvl) => (
-            <div key={lvl.id}>
-              <div>
-                {lvl.name} - {lvl.lvData}
-              </div>
-            </div>
-          ))}{" "}
-        </div>
+        <table>
+          <tbody>
+            {dataLevels.map((lvl) => (
+              <tr key={lvl.id}>
+                <td>{lvl.creator ? lvl.creator.username : null} </td>
+                <td>|</td>
+                <td>{lvl.name}</td>
+                <td>|</td>
+                <td>{lvl.lvData}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       )}
     </div>
   );
