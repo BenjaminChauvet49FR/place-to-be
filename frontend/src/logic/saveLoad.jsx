15c5f5caf1cd5104a6e0ee3f4ccccc7c7c72cdf5
Benjamin <benjchauvet@gmail.com>
2026-06-03 22:58:59 +0200

    [FRONT] Suite du fixup
    Oubli du changement de bascule, qui est nécessaire même dans ce cas particulier
    Par chance, je n'ai pas besoin de changr la fonction de chargement, seulement celle de sauvegarde
    Mais je dois quand même changer le booléen, ce que j'avais oublié de faire

--- frontend/src/logic/saveLoad.jsx
+++ frontend/src/logic/saveLoad.jsx
@@ -10,7 +10,7 @@ import {
   Error404,
 } from "../utils/api.jsx";
 
-const BASCULE_ENCODING_HAPPENING = false; // Vaut true si on est en train de basculer les niveaux de l'ancien encodage vers le nouveau
+const BASCULE_ENCODING_HAPPENING = true; // Vaut true si on est en train de basculer les niveaux de l'ancien encodage vers le nouveau
 const PREFIX_FOR_NEW_ENCODING_SYSTEM = "*";
 
 export function loadNewLevel(pDispatch) {
